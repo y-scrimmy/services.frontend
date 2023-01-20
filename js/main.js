5c9e864041075d2a296a814bfed7ae365c076098
Scrimmy <w.o.t.test@mail.ru>
2023-01-20 20:03:43 +0300

    Сверстана index страница, добавлены стили, добавлен скрипт

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var container = document.querySelector('.popup-container');
+    var popupButtons = document.querySelectorAll('.open-popup');
+    var submitBtn = document.querySelector('.form-submit');
+
+    for(let i = 0; i < popupButtons.length; i++) {
+        popupButtons[i].addEventListener('click', function() {
+            container.style.display = 'flex';
+        });
+    }
+
+    container.addEventListener('click', function(event) {
+        if(event.target == container) {
+            container.style.display = 'none';
+        }
+    });
+
+    submitBtn.addEventListener('click', function() {
+        container.style.display = 'none';
+    });
+});
